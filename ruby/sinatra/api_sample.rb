69c234a98092678fa9585eca9b73e0248f4c4cee
ryoya_ito <horusu.dqx@gmail.com>
2019-02-21 20:14:47 +0900

    sinatra パラメータ付きリクエストサンプル作成

--- ruby/sinatra/api_sample.rb
+++ ruby/sinatra/api_sample.rb
@@ -1,9 +1,20 @@
 require 'sinatra'
 require 'json'
 
+# 単純なGetリクエストを受け取る用
 get '/' do
   hello = {
       message: 'Hello World!'
+  }
+
+  hello.to_json
+end
+
+# パラメータ付きのGetリクエストを受け取る用
+get '/hello' do
+  str = params['str']
+  hello = {
+      message: "Hello #{str}"
   }
 
   hello.to_json
